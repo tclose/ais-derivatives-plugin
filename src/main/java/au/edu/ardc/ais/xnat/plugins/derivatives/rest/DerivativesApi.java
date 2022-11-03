c06eaa9ecec6a6235ec5bb9b8944ef967849d00b
Tom Close <tom.g.close@gmail.com>
2022-11-03 16:13:39 +0000

    added in screens and velocity templates to edit derivatives

---
+++ src/main/java/au/edu/ardc/ais/xnat/plugins/derivatives/rest/DerivativesApi.java
@@ -0,0 +1,10 @@
+package au.edu.ardc.ais.xnat.plugins.derivatives.rest;
+import org.nrg.framework.annotations.XapiRestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@XapiRestController
+@RequestMapping(value="/derivatives")
+public class DerivativesApi {
+    
+}
---
+++ src/main/java/org/apache/turbine/app/xnat/modules/screens/XDATScreen_report_ais_derivatives.java
@@ -0,0 +1,16 @@
+package org.apache.turbine.app.xnat.modules.screens;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+import org.nrg.xdat.turbine.modules.screens.SecureReport;
+
+/**
+ * @author XDAT
+ *
+ */
+public class XDATScreen_report_ais_derivatives extends SecureReport {
+	public static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(XDATScreen_report_ais_derivatives.class);
+	/* (non-Javadoc)
+	 * @see org.nrg.xdat.turbine.modules.screens.SecureReport#finalProcessing(org.apache.turbine.util.RunData, org.apache.velocity.context.Context)
+	 */
+	public void finalProcessing(RunData data, Context context) {
+	}}
